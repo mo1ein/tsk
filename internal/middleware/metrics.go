255160e4b7b064dfefe81e00e74dbced3803cbc0
moein <moeinn.com@gmail.com>
2026-07-21 16:30:00 +0330

    feat: add Prometheus metrics and request middleware

---
+++ internal/middleware/metrics.go
@@ -0,0 +1,43 @@
+package middleware
+
+import (
+	"strconv"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+)
+
+var (
+	requestsTotal = promauto.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "http_requests_total",
+			Help: "Total number of HTTP requests",
+		},
+		[]string{"method", "path", "status"},
+	)
+
+	requestLatency = promauto.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Name:    "http_request_latency_seconds",
+			Help:    "HTTP request latency in seconds",
+			Buckets: prometheus.DefBuckets,
+		},
+		[]string{"method", "path"},
+	)
+)
+
+func MetricsMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		start := time.Now()
+
+		c.Next()
+
+		duration := time.Since(start).Seconds()
+		status := strconv.Itoa(c.Writer.Status())
+
+		requestsTotal.WithLabelValues(c.Request.Method, c.FullPath(), status).Inc()
+		requestLatency.WithLabelValues(c.Request.Method, c.FullPath()).Observe(duration)
+	}
+}
